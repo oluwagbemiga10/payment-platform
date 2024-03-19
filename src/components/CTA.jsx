f585cbf6c71ef7bd4f8b6f20238d29333d6b5f30
gbenga <oyelamigbenga11@gmail.com>
2024-03-19 03:21:26 +0100

    some_modifications

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -13,7 +13,7 @@ const CTA = () => (
         </p>
       </div>
       <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-        <Button />
+        <Button/>
       </div>
     </section>
   )
